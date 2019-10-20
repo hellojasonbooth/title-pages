4e4b07076ff8c045e31df3307a7ccb33962dc44f
Jason Booth <hello@studiojasonbooth.com>
2019-10-20 14:52:14 +0100

    added menu transition

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,1 +1,22 @@
 // lets put some code here
+
+
+
+// menu toggler
+const menuToggleTag = document.querySelector('div.burger-menu')
+// menu
+const menuTag = document.querySelector('div.menu')
+
+
+menuToggleTag.addEventListener('click', function(){
+   //
+   menuTag.classList.toggle('open')
+   menuToggleTag.classList.toggle('active')
+
+   // this is where we can add the blur to elements
+   // when menu is open and remove the blur when
+   // menu is closed
+
+})
+
+
