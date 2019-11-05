03a1521ad00dfa80d538119c137e3dfb7225b662
Jason Booth <hello@studiojasonbooth.com>
2019-11-05 13:45:28 +0000

    added blur when menu opens

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,30 +1,6 @@
 // lets put some code here
 
 
-
-// menu toggler
-const burgerTag = document.querySelector('div.burger-menu')
-const menuToggleTag = document.querySelector('header')
-// menu
-const menuTag = document.querySelector('div.menu')
-
-
-menuToggleTag.addEventListener('click', function(){
-   //
-   menuTag.classList.toggle('open')
-   burgerTag.classList.toggle('active')
-
-   // this is where we can add the blur to elements
-   // when menu is open and remove the blur when
-   // menu is closed
-
-   // if (menuToggleTag.classList.contains('active')) {
-
-   // }
-
-})
-
-
 // stickers
 let number = 0
 
@@ -44,10 +20,11 @@ const addSticker = function (x, y) {
 
    const img = document.createElement("img")
    img.setAttribute("src", stickers[number])
-   img.classList.add("mystyle")
+   img.classList.add("sticker-slapped")
    img.style.transform = "rotate(" + randomNum + "deg) translateX(" + (-50) + "%) translateY(" + (-50) + "%)"
    stickerTag.appendChild(img)
-   
+
+   // remove half the window width
    img.style.left = x + "px"
    img.style.top = y + "px"
 
@@ -62,6 +39,43 @@ const addSticker = function (x, y) {
 
 stickerTag.addEventListener("click", function (event){
    addSticker(event.pageX, event.pageY)
+})
+
+
+
+
+// menu toggler
+const burgerTag = document.querySelector('div.burger-menu')
+const menuToggleTag = document.querySelector('header')
+// menu
+const menuTag = document.querySelector('div.menu')
+
+const bg = document.querySelector('body')
+
+
+menuToggleTag.addEventListener('click', function(){
+   //
+   menuTag.classList.toggle('open')
+   burgerTag.classList.toggle('active')
+
+   // this is where we can add the blur to elements
+   // when menu is open and remove the blur when
+   // menu is closed
+
+   const blurredImageTag = document.querySelectorAll('img, h1')
+
+   blurredImageTag.forEach(tag => {
+
+      if (menuTag.classList.contains('open')) {
+         tag.classList.add('blurred')
+      } else {
+         tag.classList.remove('blurred')
+      }
+
+     })
+
+
+
 })
 
 
